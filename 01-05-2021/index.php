e3c78896b0eaed1d0a94bdf82dc28d8520b8961f
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-05-01 23:27:06 -0300

    Add files via upload

---
+++ 01-05-2021/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css" />
+    <title>Arena de Cartas</title>
+</head>
+<body>
+    <div id="Arena_de_Cartas">
+        <div id="decks_inimigo">
+            <div class="deck_inimigo"></div>
+            <div class="deck_inimigo"></div>
+            <div class="deck_inimigo"></div>
+            <div class="deck_inimigo"></div>
+            <div class="deck_inimigo"></div>
+            <div class="deck_inimigo"></div>
+            <div class="deck_inimigo"></div>
+            <div class="deck_inimigo"></div>
+            <div class="deck_inimigo"></div>
+            <div class="deck_inimigo"></div>
+        </div>
+        <div id="setar_cartas">
+            <div class="cartas">
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+                <div class="carta"></div>
+            </div>
+        </div>
+        <div id="afavor">
+            <div id="decks_aliado">
+                <div class="deck_aliado"></div>
+                <div class="deck_aliado"></div>
+                <div class="deck_aliado"></div>
+                <div class="deck_aliado"></div>
+                <div class="deck_aliado"></div>
+                <div class="deck_aliado"></div>
+                <div class="deck_aliado"></div>
+                <div class="deck_aliado"></div>
+                <div class="deck_aliado"></div>
+            </div>
+            <div id="seu_deck"></div>
+        </div>
+    <div>
+</body>
+</html>
