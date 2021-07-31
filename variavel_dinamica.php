ceb5dae09a6f18002577e4efd922aa94896086fa
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-07-31 19:05:39 -0300

    Add files via upload

---
+++ variavel_dinamica.php
@@ -0,0 +1,13 @@
+<?php
+//https://www.php.net/manual/pt_BR/language.variables.variable.php
+
+//Variavel $nome recebe o valor “variavel”
+$nome = 'variavel';
+
+//Declaração da nova variável. O identificador será o conteúdo de $nome
+$$nome = 'DevMedia';
+
+//Exibe o conteúdo de $variavel
+echo $variavel;
+
+?>
