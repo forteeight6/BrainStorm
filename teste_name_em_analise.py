f7e39b6323a93f777c4860a9b364cafc03504c8f
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-05 07:33:10 -0300

    Add files via upload

---
+++ teste_name_em_analise.py
@@ -0,0 +1,7 @@
+# fonte: https://www.alura.com.br/artigos/o-que-significa-if-name-main-no-python
+
+print(__name__)
+
+# ISSO É UMA TÉCNICA POREM AINDA NÃO HA ENTENDI DIREITO
+if __name__ == '__main__':
+    print('teste')
