debe2d0c158bdddf64776fb65e34c9e3ccc1a653
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-04 21:25:16 -0300

    Add files via upload

---
+++ modulo_importelib_absoluto_ok.py
@@ -0,0 +1,6 @@
+# fonte: https://stackoverflow.com/questions/10675054/how-to-import-a-module-in-python-with-importlib-import-module
+import importlib
+
+obj = importlib.import_module('modulos.b.a.c.modulo')
+
+obj.text()
