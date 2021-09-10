f6bafbfbf0779ff23aa1363907cf095b512b50a1
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-10 06:56:40 -0300

    Add files via upload

---
+++ trabalhando_com_diretorio.py
@@ -0,0 +1,19 @@
+# https://www.youtube.com/watch?v=m8orGLxO03s&ab_channel=Ot%C3%A1vioMiranda
+
+# https://www.youtube.com/watch?v=6a4m7L7juKQ
+
+import os
+import shutil
+
+origem = 'C:/Users/dyego/Desktop/Cronos/ambiente_de_idealizacao/Micro-sistema - Exercicios_de_testes_comportamentais/scripts/0 - Comportamento_em_construcao/'
+destino = 'C:/Users/dyego/Desktop/Cronos/ambiente_de_idealizacao/Micro-sistema - Exercicios_de_testes_comportamentais/scripts/0 - Comportamento_em_construcao/teste/'
+
+try:
+    os.mkdir(destino)
+except FileExistsError as e:
+    print(f'Pasta {destino} já existe.')
+    # pass
+
+for root, dirs, files in os.walk(origem):
+    for file in files:
+        print(file)
