398e6b23812364f8cd448fa0325c3b314bc988cc
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-11 06:50:00 -0300

    Add files via upload

---
+++ trabalhando_com_lista_6.py
@@ -0,0 +1,8 @@
+# Ordenando Listas.
+lista = [7, 2, 3, 7, -1, 9]
+print("Lista original antes da ordenação: ", lista)
+
+lista_ordenada = sorted(lista)
+print(f"Lista Ordenada: {lista_ordenada}")
+
+print(f"A lista original permanece inalterada: {lista}")
