417a8cf14b6ebd0abdd2d2195bb33ef91191aab6
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-11 06:49:48 -0300

    Add files via upload

---
+++ trabalhando_com_lista_5.py
@@ -0,0 +1,10 @@
+# Ordenando listas.
+
+lista = [7, 25, 2, 3, 30, 7, 80, 100, -1, 15]
+print(f"Lista não Ordenada: {lista}")
+
+lista.sort()
+print(f"Lista Ordenada: {lista}")
+
+lista.sort(reverse=True)
+print(f"Lista Ordenada em ordem descrescente: {lista}")
