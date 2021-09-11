d341ae1c221dcfd64e18d7566a3f2140e0ae1703
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-11 06:50:11 -0300

    Add files via upload

---
+++ trabalhando_com_lista_7.py
@@ -0,0 +1,19 @@
+# Cria uma lista vazia.
+lista = []
+
+# Adiciona elementos no final da lista.
+lista.append("P")
+lista.append("y")
+lista.append("t")
+lista.append("h")
+lista.append("o")
+lista.append("n")
+
+print(lista)
+
+lista[1] = "a"
+lista[3] = "r"
+lista[4] = "i"
+lista[5] = "a"
+
+print(lista)
