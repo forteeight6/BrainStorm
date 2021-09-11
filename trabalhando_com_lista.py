f02efa7df651489a2e98b6f15ed35d31466e275a
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-11 06:47:50 -0300

    Add files via upload

---
+++ trabalhando_com_lista.py
@@ -0,0 +1,13 @@
+# Cria uma lista sem nenhum elemente.
+
+# A expressão lista_vazia = list() possui o mesmo efeito.
+
+lista_vazia = []
+print("Lista vazia: ", lista_vazia)
+print("Tipo de uma lista: ", type(lista_vazia))
+
+lista_inteiros = [10, 20, 30, 40]
+print("Lista de inteiros: ", lista_inteiros)
+
+lista_tipos_diferentes = ["George", "Orwell", 1984]
+print("Lista de elementos com tipos diferentes: ", lista_tipos_diferentes)
