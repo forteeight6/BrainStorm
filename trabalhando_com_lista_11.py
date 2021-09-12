58c68821f718d8e6074f4af7dc5d8441066d837f
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-12 06:55:32 -0300

    Add files via upload

---
+++ trabalhando_com_lista_11.py
@@ -0,0 +1,8 @@
+# Removendo um elemento em uma posição especifica usamos del.
+
+lista = ['A', 'S', 'C', 'E', 'N', 'D', 'E', 'R']
+print(f"Lista Original: {lista}")
+
+
+del lista[1]
+print("Removendo um elemento: ", lista)
