0af149082cd4299b0482e0fcb9abc3ffc219ed31
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-13 06:57:38 -0300

    Add files via upload

---
+++ alguns_exercicios.py
@@ -0,0 +1,14 @@
+# Crie uma Função que recebe uma lista ou string x e retorna x concatenada com ela mesma.
+
+def concatena(x):
+    return x + x
+
+
+def concatena2(x):
+    return 2 * x
+
+
+lista = [2, 4, 8]
+print(concatena('DI'))
+print(concatena2('lu'))
+print(concatena2(lista))
