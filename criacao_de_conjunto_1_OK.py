441504a923416a9aa2d192698abe7e7c301b1758
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-21 11:02:21 -0300

    Add files via upload

---
+++ criacao_de_conjunto_1_OK.py
@@ -0,0 +1,8 @@
+# Python provê formas eficientes e convenientes para criação e manipulação de sets. Nesta seção aprenderemos as principais características e funcionalidades dessa poderosa estrutura de dados em Python.
+# Um set em Python é uma coleção de itens únicos (distintos).
+
+# Exemplo de criação de sets.
+numeros = [1, 2, 2, 3, 3, 3]
+numeros_distintos = set(numeros)
+print("Números: ", numeros)
+print("Números distintos: ", numeros_distintos)
