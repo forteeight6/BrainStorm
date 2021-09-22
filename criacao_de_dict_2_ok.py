96a0c1351ad1e5812d2ca7df2709f074a9836f35
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-22 08:43:22 -0300

    Add files via upload

---
+++ criacao_de_dict_2_ok.py
@@ -0,0 +1,13 @@
+# Modificando dicionários em Python
+# Ao contrário de tuplas, dicionários podem ser modificados:
+
+# Modificando um dicionário.
+paises = dict()
+
+paises["BRA"] = "Brazil"
+paises["FRA"] = "France"
+
+# Adicionando um elemento.
+paises["ESP"] = "Espanha"
+
+print("Dicionário modificado: ", paises)
