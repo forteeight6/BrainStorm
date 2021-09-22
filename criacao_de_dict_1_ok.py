ad0b4daecc057c032aac451c248ae9a18c0abcfc
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-22 08:43:12 -0300

    Add files via upload

---
+++ criacao_de_dict_1_ok.py
@@ -0,0 +1,12 @@
+# Dicionários em Python são conjuntos de chave-valor. Assim como um dicionário chaves (termos) que estão associadas a valores (significados dos termos), dicionários em Python são uma estrutura de dados que nos permite mapear chaves a valores.
+
+# A sintaxe para criação de dicionários em Python é {chave1 : valor1, chave2 : valor2, …​}.
+
+# O comando dicionario_vazio = dict() possui o mesmo efeito do comando abaixo.
+dicionario_vazio = {}
+print("Dicionário vazio: ", dicionario_vazio)
+
+paises = {'BRA': 'Brasil', 'EUA': 'Estados Unidos', 'FRA': 'França'}
+print("Exemplo de dicionário: ", paises)
+
+print("Tipo de um dicionário: ", type(paises))
