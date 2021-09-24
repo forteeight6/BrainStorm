728d193ae25847192aed0a02e1a52f2f701fcbb3
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-24 09:08:09 -0300

    Add files via upload

---
+++ objeto_python_6_ok.py
@@ -0,0 +1,38 @@
+# Palavras reservadas
+# É importante ressaltar que nem toda palavra pode ser usada como nome de uma variável em Python. Existem algumas regras.
+
+# Nomes de variáveis em Python podem conter caracteres alfanuméricos (a-z, A-Z, 0-9) e alguns caracteres especiais, como o _.
+
+# Por convenção, nomes de variáveis começam com letra minúscula.
+
+# Existem palavras reservadas em Python (elas são parte do vocabulário da linguagem), que não podem ser usadas como nomes de variáveis:
+
+# nd, as, assert, break, class, continue, def, del, elif, else, except,
+# exec, finally, for, from, global, if, import, in , is , lambda, not, or ,
+# pass, print, raise, return, try, while, with, yield
+
+# Operadores Aritméticos
+
+# Soma e subtração
+print(10 + 2 - 7)
+
+# Potência: 2 ^ 4
+print(2 ** 4)
+
+# Divisão de ponto flutuante
+print(20 / 6)
+
+# Divisão inteira, sem considerar o resto
+print(20 // 6)
+
+# Resto da divisão
+print(20 % 6)
+
+# Operadores Lógicos
+# and, or, not
+
+# Operadores Relacionais
+# Python possui também operadores relacionais (de comparação), como os da matemática: <, >, ⇐, >=, ==, !=.
+
+# Operadores de Atribuição
+# =, -=, =, /=, //=, %=, *=
