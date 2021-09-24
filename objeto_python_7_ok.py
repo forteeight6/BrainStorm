8d070484927307a8f05650fff62bb403c9570a58
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-24 09:08:22 -0300

    Add files via upload

---
+++ objeto_python_7_ok.py
@@ -0,0 +1,20 @@
+# Qual será o resultado da expressão abaixo?
+
+print(2 + 3 * 5 + 30 // 10)
+
+
+# Podemos colocar parenteses na expressão para que fique mais facil entendermos a ordem em que as operações serão executadas. A expressão acima é equivalente a:
+
+# 2 + (3 * 5) + (30 // 10)
+
+# Neste exercício, importante é saber que a multiplicação e a divisão serão executadas antes das somas. A multiplicação será executada antes da divisão não porque possui maior precedência, mas porque aparece primeiro na expressão. No final, teremos o seguinte:
+
+# 2 + (3 * 5) + (30 // 10)
+
+# 2 + 15 + (30 // 10)
+
+# 2 + 15 + 3
+
+# 17 + 3
+
+# 20
