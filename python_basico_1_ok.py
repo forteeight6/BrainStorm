cb12090c20c0dccc26553c1112591a70e4720076
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-26 13:53:40 -0300

    Add files via upload

---
+++ python_basico_1_ok.py
@@ -0,0 +1,22 @@
+# Números inteiros
+x = 1
+y = 2
+resultado = x + y
+print(resultado)
+print(type(resultado))
+
+# Números de ponto flutuante
+x = 1.1
+y = 2.2
+resultado = x + y
+print(resultado)
+print(type(resultado))
+
+x = 10
+y = 2.5
+resultado = x * y
+print(resultado)
+print(type(resultado))
+
+
+# A função type() retorna o tipo do valor passado como parâmetro. No nosso exemplo, como a soma x + y possui um valor inteiro, a função type() retorna <class 'int'> indicando que o valor é de fato um número inteiro.
