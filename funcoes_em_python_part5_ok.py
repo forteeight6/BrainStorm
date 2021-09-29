272b876238398016071068fd8f23632497a6c2b3
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-29 08:35:47 -0300

    Add files via upload

---
+++ funcoes_em_python_part5_ok.py
@@ -0,0 +1,11 @@
+# Escreva uma função para imprimir o valor absoluto de um número.
+
+def imprime_valor_absoluto(n):
+    if n < 0:
+        n = -n
+    print(n)
+
+
+imprime_valor_absoluto(-5)
+imprime_valor_absoluto(10)
+imprime_valor_absoluto(-1)
