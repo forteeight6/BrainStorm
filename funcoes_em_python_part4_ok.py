7db54feed06c30a5fd89c8547a2125089ec85f26
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-29 08:35:38 -0300

    Add files via upload

---
+++ funcoes_em_python_part4_ok.py
@@ -0,0 +1,12 @@
+# Escreva uma função que recebe um número n como parâmetro e imprime se n é positivo ou negativo.
+
+
+def imprime_sinal(n):
+    if n < 0:
+        print("Negativo")
+    else:
+        print("Positivo")
+
+
+imprime_sinal(-1)
+imprime_sinal(5)
