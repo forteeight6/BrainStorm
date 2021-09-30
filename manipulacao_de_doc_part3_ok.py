ebfb7f362438da0343b339433ddf669796ca564a
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-09-30 09:16:49 -0300

    Add files via upload

---
+++ manipulacao_de_doc_part3_ok.py
@@ -0,0 +1,7 @@
+# Se o arquivo estiver localizado em um local diferente, você terá que especificar o caminho do arquivo, assim:
+
+# Exemplo
+# Abra um arquivo em um local diferente:
+
+f = open("C:\\Users\dyego\Desktop\Cronos\\ambiente_de_idealizacao\Micro-sistema - Logica\Scripts_de_Logica\\0 - Comportamento_em_construcao\\teste1\\teste.txt", "r")
+print(f.read())
