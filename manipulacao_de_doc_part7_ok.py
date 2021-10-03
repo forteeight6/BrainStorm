1eb4444391c8495b33fa78b5ef4d29781732fa4b
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-10-03 19:28:18 -0300

    Add files via upload

---
+++ manipulacao_de_doc_part7_ok.py
@@ -0,0 +1,8 @@
+# Ao percorrer as linhas do arquivo, você pode ler todo o arquivo, linha por linha:
+
+# Exemplo
+# Percorra o arquivo linha por linha:
+
+f = open("teste.txt", "r")
+for x in f:
+    print(x)
