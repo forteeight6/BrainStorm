fee36fb2b416d2ae24273307ad6e8bbc141173e4
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-10-03 19:28:11 -0300

    Add files via upload

---
+++ manipulacao_de_doc_part6_ok.py
@@ -0,0 +1,12 @@
+# Ligando readline()duas vezes, você pode ler as duas primeiras linhas:
+
+# Exemplo
+# Leia duas linhas do arquivo:
+
+f = open("teste.txt", "r")
+print(f.readline())
+print(f.readline())
+
+print(f)
+# não sei porque mas esta executando apenas pelo comando via terminal
+# python nome do script
