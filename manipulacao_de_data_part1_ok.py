872e9ee5b78ba4cfac61817a6bfd9a6c8614fd97
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-10-04 08:33:21 -0300

    Add files via upload

---
+++ manipulacao_de_data_part1_ok.py
@@ -0,0 +1,10 @@
+# Datas Python
+# Uma data em Python não é um tipo de dados em si, mas podemos importar um módulo denominado datetimepara trabalhar com datas como objetos de data.
+
+# Exemplo
+# Importe o módulo datetime e exiba a data atual:
+
+import datetime
+
+x = datetime.datetime.now()
+print(x)
