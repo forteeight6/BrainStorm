b5a38fef5cd60948046f476ff5b14191bd7d6624
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-10-11 11:06:44 -0300

    Add files via upload

---
+++ dicionario_de_orgãos_do_corpo_humano.py
@@ -0,0 +1,19 @@
+# Fonte: https://brasilescola.uol.com.br/biologia/corpo-humano.htm
+orgaos_do_corpo_humano = {
+    'Bexiga': 'local de armazenamento da urina após a formação desse produto pelos rins.',
+    'Coracao': 'órgão responsável por impulsionar o sangue para o corpo. Graças a esse bombeamento, as células conseguem adquirir oxigênio e outros nutrientes necessários.',
+    'Esôfago': 'tubo muscular que garante que o alimento seja levado da boca para o estômago.',
+    'Estomago': 'órgão do sistema digestório em que parte da digestão acontece. Ele é responsável por produzir o suco gástrico e transformar o bolo alimentar em quimo.',
+    'Intestino delgado': 'onde acontece o fim do processo de digestão e a absorção de grande parte dos nutrientes retirados dos alimentos.',
+    'Intestino grosso': 'onde acontece a absorção da água e a formação das fezes.',
+    'Laringe': 'esse órgão do sistema respiratório une a faringe à traqueia. É na laringe que se encontram as pregas vocais.',
+    'Ovários': 'órgãos exclusivos das mulheres onde são produzidos gametas femininos e hormônios sexuais femininos.',
+    'Pâncreas': 'glândula mista responsável por produzir suco pancreático e dois importantes hormônios (insulina e glucagon), que atuam na regulação da taxa de glicose no sangue.',
+    'Pulmões': 'órgãos esponjosos do sistema respiratório ricos em alvéolos, que são os locais onde ocorrem as trocas gasosas.',
+    'Rins': 'órgãos do sistema urinário onde é produzida a urina.',
+    'Testículo': 'órgãos exclusivos dos homens onde são produzidos os gametas masculinos e os hormônios sexuais masculinos.',
+    'Tubas uterinas': 'parte do sistema reprodutor feminino onde geralmente ocorre a fecundação.',
+    'Útero': 'parte do sistema reprodutor feminino onde o embrião desenvolve-se durante a gravidez.'
+}
+
+print(orgaos_do_corpo_humano)
