f5e46ae966b99992919f1a9d5740554f342aec7c
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-10-11 07:18:22 -0300

    Add files via upload

---
+++ aprendizado_de_maquina_part1_ok.py
@@ -0,0 +1,85 @@
+# Aprendizado de Máquina
+
+# O aprendizado de máquina está fazendo com que o computador aprenda com o estudo de dados e estatísticas.
+
+# O aprendizado de máquina é um passo na direção da inteligência artificial (IA).
+
+# Aprendizado de máquina é um programa que analisa dados e aprende a prever o resultado.
+
+# Onde começar?
+# Neste tutorial, vamos voltar para matemática e estatística de estudo, e como calcular números importantes com base em conjuntos de dados.
+
+# Também aprenderemos como usar vários módulos Python para obter as respostas de que precisamos.
+
+# E aprenderemos como fazer funções capazes de prever o resultado com base no que aprendemos.
+
+# Conjunto de Dados
+# Na mente de um computador, um conjunto de dados é qualquer coleção de dados. Pode ser qualquer coisa, desde um array até um banco de dados completo.
+
+# Exemplo de uma matriz:
+
+# [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+# Exemplo de banco de dados:
+
+# Carname	Cor		Era	Velocidade	AutoPass
+# --------------------------------------------------------------
+# BMW		vermelho	5	99		Y
+# --------------------------------------------------------------
+# Volvo		Preto		7	86		Y
+# --------------------------------------------------------------
+# VW		cinzento	8	87		N
+# --------------------------------------------------------------
+# VW		Branco		7	88		Y
+# --------------------------------------------------------------
+# Ford		Branco		2	111		Y
+# --------------------------------------------------------------
+# VW		Branco		17	86		Y
+# --------------------------------------------------------------
+# Tesla		vermelho	2	103		Y
+# --------------------------------------------------------------
+# BMW		Preto		9	87		Y
+# --------------------------------------------------------------
+# Volvo		cinzento	4	94		N
+# --------------------------------------------------------------
+# Ford		Branco		11	78		N
+# --------------------------------------------------------------
+# Toyota	cinzento	12	77		N
+# --------------------------------------------------------------
+# VW		Branco		9	85		N
+# --------------------------------------------------------------
+# Toyota	azul		6	86		Y
+# --------------------------------------------------------------
+
+# Olhando para a matriz, podemos adivinhar que o valor médio está provavelmente em torno de 80 ou 90, e também podemos determinar o valor mais alto e o valor mais baixo, mas o que mais podemos fazer?
+
+# E olhando para o banco de dados, podemos ver que a cor mais popular é o branco, e o carro mais antigo tem 17 anos, mas e se pudéssemos prever se um carro tivesse um AutoPass, apenas olhando para os outros valores?
+
+# É para isso que serve o Aprendizado de Máquina! Analisando dados e prevendo o resultado!
+
+# No Aprendizado de Máquina é comum trabalhar com conjuntos de dados muito grandes. Neste tutorial, tentaremos facilitar ao máximo o entendimento dos diferentes conceitos de aprendizado de máquina e trabalharemos com pequenos conjuntos de dados fáceis de entender.
+
+# Tipos de dados
+# Para analisar os dados, é importante saber com que tipo de dados estamos lidando.
+
+# Podemos dividir os tipos de dados em três categorias principais:
+
+# Numérico
+# Categórico
+# Ordinal
+# Os dados numéricos são números e podem ser divididos em duas categorias numéricas:
+
+# Dados discretos
+
+# - números limitados a inteiros. Exemplo: o número de carros que passam.
+
+# Dados contínuos
+
+# - números de valor infinito. Exemplo: o preço de um item ou o tamanho de um item
+# Os dados categóricos são valores que não podem ser comparados uns com os outros. Exemplo: um valor de cor ou quaisquer valores sim / não.
+
+# Os dados ordinais são como dados categóricos, mas podem ser comparados uns com os outros. Exemplo: notas escolares em que A é melhor que B e assim por diante.
+
+# Conhecendo o tipo de dados de sua fonte de dados, você saberá qual técnica usar para analisá-los.
+
+# Você aprenderá mais sobre estatísticas e análise de dados nos próximos capítulos.
