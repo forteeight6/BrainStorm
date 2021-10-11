8371d5b8a15159cf9d9e18f42c21e5c4bc754eed
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-10-11 11:06:31 -0300

    Add files via upload

---
+++ dicionario_de_celulas_do_corpo_humano.py
@@ -0,0 +1,8 @@
+# Combinação Dicionario de Termos de Biologia.
+# Tema: Partes do Corpo Humano.
+# fonte: https://brasilescola.uol.com.br/biologia/corpo-humano.htm
+
+celulas_do_corpo_humano = {'Adipócitos': 'células que armazenam gorduras.', 'Célula de Schwann': ' produzem a bainha de mielina dos neurônios.', 'Células beta': 'células do pâncreas responsáveis por produzir insulina.', 'Condroblastos': 'células jovens do tecido cartilaginoso que formam a matriz da cartilagem', 'Condrócitos': 'células do tecido cartilaginoso, derivadas dos condroblastos, que ocupam lacunas na matriz da cartilagem.',
+                           'Espermatozoides': 'gametas masculinos', 'Hemácias, eritrócitos ou glóbulos vermelhos': 'células sanguíneas que garantem o transporte de oxigênio pelo corpo.', 'Hepatócitos': 'células do fígado que sintetizam proteínas e a bile, além de garantir a detoxificação de várias substâncias.', 'Leucócitos ou glóbulos brancos': 'células sanguíneas que atuam na defesa do organismo. Os tipos de leucócitos existentes são neutrófilos, eosinófilos, basófilos, linfócitos e monócitos.', 'Neurônios': 'células responsáveis pela transmissão dos impulsos nervosos.', 'Osteoblastos': 'células do tecido ósseo responsáveis por produzir a parte orgânica da matriz óssea.', 'Osteócitos': 'células maduras do tecido ósseo que derivam de osteoblastos e são encontradas em lacunas na matriz óssea.', 'Osteoclastos': 'células do tecido ósseo que atuam na reabsorção desse tecido', 'Ovócito': 'gameta feminino'}
+
+print("Celulas do Corpo Humano: ", celulas_do_corpo_humano)
