9506d3d3970eb5fb25d9d9741c459b163d1cbad6
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-10-19 09:25:06 -0300

    Add files via upload

---
+++ copia_e_renomeia_diretorio.py
@@ -0,0 +1,8 @@
+nome_da_materia = 'matematica'
+topico_da_materia = 'divisao'
+tipo_da_materia = 'calculos 1'
+titulo_do_exercicio = 'dividindo uma pizzas'
+
+diretorio = f'C:/Users/dyego/Desktop/Gerenciador de Exercicios/databases/{nome_da_materia}/{topico_da_materia}/{tipo_da_materia}/{titulo_do_exercicio}/Descricao/descricao_1.txt'
+
+print(diretorio)
