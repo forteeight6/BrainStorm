be0bc8f97edd6fb77b16a60a675c51790db7b271
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-10-21 08:38:32 -0300

    Add files via upload

---
+++ derivado_pyautogui_1_3.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+pyautogui.click(600, 600)
+pyautogui.typewrite('graphicsnotes')
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.typewrite('Ongs')
+pyautogui.press('enter')
+time.sleep(2)
