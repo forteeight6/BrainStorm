0dec64f565edc2a23cd4b9dcebe73dd1e872d67e
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-10-21 08:38:21 -0300

    Add files via upload

---
+++ derivado_pyautogui_1_2.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+# Movimenta o mouse para uma determinada posição.
+pyautogui.click(600, 500)
+
+# Digita no teclado o texto passado como parametro.
+pyautogui.typewrite('graphicsnotes')
