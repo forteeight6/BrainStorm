3d9dbb9a91139025d6e8406d8244ac865ef896b1
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-10-22 08:40:49 -0300

    Add files via upload

---
+++ derivado_pyautogui_2_2.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+# python derivado_pyautogui_2_2.py
+currentMouseX, currentMouseY = pyautogui.position()
+
+print(f'currentMouseX = {currentMouseX}')
+print(f'currentMouseY = {currentMouseY}')
