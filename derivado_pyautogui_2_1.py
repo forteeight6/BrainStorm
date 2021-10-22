c126b12cd3b60d863cec69727d401985e930b15b
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-10-22 08:40:39 -0300

    Add files via upload

---
+++ derivado_pyautogui_2_1.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+# python derivado_pyautogui_2_1.py
+screenWidth, screenHeight = pyautogui.size()
+
+print(f'screenWidth = {screenWidth}')
+print(f'screenHeight = {screenHeight}')
