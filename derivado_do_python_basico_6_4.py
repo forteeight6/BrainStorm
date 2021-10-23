c060d2bbd3aa0cd050bbf0ff92cc8ee78e1a0dc5
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-10-23 04:42:33 -0300

    Add files via upload

---
+++ derivado_do_python_basico_6_4.py
@@ -0,0 +1,4 @@
+print('usando a função enumarate para acessar o indice do intervalo')
+for indice, elemento in enumerate(range(-3, 3)):
+    print(indice, elemento)
+print('usando a função enumarate para acessar o indice do intervalo')
