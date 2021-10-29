b5c19fbf7364dbf4f4c6b6feb9106e1769324b48
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-10-29 09:50:36 -0300

    Add files via upload

---
+++ solucao_part1.py
@@ -0,0 +1,17 @@
+Listagem de Processos
+
+1 - Acessar www.tradingview.com;
+2 - Clicar em Mercados;
+3 - Clicar em Ações;
+4 - Clicar em Qualquer opção que aparecer;
+5 - Selecionar a ação que desejar;
+6 - Clicar em Gráfico completo;
+7 - Exportar dados do rastreador para um arquivo CSV.
+
+* A cada 10 segundos ele atualiza os dados da bolsa de valores do Brasil.
+* Exportar dados do rastreador para um arquivo CSV.
+
+Modulos para Web scrapping
+Scrapy - refresh wait 1s|extremamente otimizado para performance porem simples.
+beautifulsoup - intermediario
+selenium - bastante completo, realmente imita o usuario.
