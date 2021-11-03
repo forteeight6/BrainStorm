322fa5b970d79893360d3dd596efe514dcdf2c0e
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-11-03 20:22:28 -0300

    Add files via upload

--- solucao.py
+++ solucao.py
@@ -1,21 +1,13 @@
-# diretorio_modelo = "nome_da_materia/topico_da_materia/tipo_do_exercicio/titulo_do_exercicio"
-# local_de_descricao = "descricoes/"
-# local_de_solucoes = "solucoes/"
-import os
-import random
-cronograma_modelo = 'cronograma_modelo.txt'
-"""
-dados = {
-    '04:00': ['Fazer Café da Manhã', 'Fazer Anki', 'Fazer Duolingo'],
-    '05:00': ['Voltar a dormir', 'Acordar 05:35', 'Levar Mozçao ao trabalho']
-}
-"""
-aleatorio = random.randint(1, 1000)
+from pandas import read_excel, DataFrame
 
-arquivo = open(cronograma_modelo, 'a')
-arquivo.close()
-os.system(f"copy {cronograma_modelo} copias")
-arquivo = open(f"copias/{cronograma_modelo}", 'a')
-# arquivo.write(dados) Nãao funcionou pois é incompativel.
-arquivo.write(str(aleatorio))
-arquivo.close()
+produtos = {
+    'Nome': ['Sabao em Po', 'Vinagre', 'Livro'],
+    'Quantidade': [2, 5, 8],
+    'Preco': [18.50, 2.50, 35.80]
+}
+
+formatar = DataFrame(data=produtos)
+formatar.to_excel('produtos.xlsx', index=False)
+print(read_excel('produtos.xlsx'))
+
+# Acertei
