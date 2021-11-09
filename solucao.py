a8e2fc8b6424a1715dfe85be51039315f174c137
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-11-09 15:54:45 -0300

    Add files via upload

--- solucao.py
+++ solucao.py
@@ -1,17 +1,29 @@
-from random import shuffle
-alunos = []
-indice = 0
-count = 1
-print('Aperte X para parar.\n')
-while True:
-    alunos.append(str(input('Digite o nome do aluno: ')))
-    if alunos[indice] == 'x':
-        break
-    print(alunos[indice])
-    indice += count
+from random import choice, shuffle
 
-shuffle(alunos)
-ordem = 0
-for aluno in alunos:
-    print(f'{ordem}º {aluno}')
-    ordem += 1
+# Insira uma lista de nomes, e a quantidade que deseja selecionar
+
+
+def selection(lista, quantidade):
+    escolhido = []
+
+    for index in range(quantidade):
+        shuffle(lista)
+        escolhido.append(choice(lista))
+
+    return escolhido
+    # print(escolhido)
+
+
+# selection(['a', 'b', 'c'], 2)
+
+alunos = []
+index = 0
+print('Insira X para finalizar o processo: \n')
+while True:
+    alunos.append(str(input('Qual o nome do alunos? ')))
+    if alunos[index].lower() == 'x':
+        break
+    index += 1
+
+for aluno in selection(alunos, 2):
+    print(f'{aluno}')
