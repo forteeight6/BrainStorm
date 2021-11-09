2164fce9f8789d31cca046a761da95a5a1d4b6f8
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-11-09 09:36:14 -0300

    Add files via upload

--- solucao.py
+++ solucao.py
@@ -1,13 +1,17 @@
-from pandas import read_excel, DataFrame
+from random import shuffle
+alunos = []
+indice = 0
+count = 1
+print('Aperte X para parar.\n')
+while True:
+    alunos.append(str(input('Digite o nome do aluno: ')))
+    if alunos[indice] == 'x':
+        break
+    print(alunos[indice])
+    indice += count
 
-produtos = {
-    'Nome': ['Sabao em Po', 'Vinagre', 'Livro'],
-    'Quantidade': [2, 5, 8],
-    'Preco': [18.50, 2.50, 35.80]
-}
-
-formatar = DataFrame(data=produtos)
-formatar.to_excel('produtos.xlsx', index=False)
-print(read_excel('produtos.xlsx'))
-
-# Acertei
+shuffle(alunos)
+ordem = 0
+for aluno in alunos:
+    print(f'{ordem}º {aluno}')
+    ordem += 1
