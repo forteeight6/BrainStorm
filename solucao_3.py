84df19493fa7baa914812f362a7fb4cab8deccad
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-11-14 11:44:45 -0300

    Add files via upload

--- solucao_3.py
+++ solucao_3.py
@@ -1,45 +1,24 @@
-class Flag:
-
-    def __init__(self):
-        with open('flag.txt', 'r') as file:
-            self.data = int(file.read().replace('\n', ''))
-
-    def add(self, num=1):
-        self.num = num
-        self.data += num
-
-        with open('flag.txt', 'w') as file:
-            file.write(f'{self.data}')
-        with open('flag.txt', 'r') as file:
-            self.data = int(file.read().replace('\n', ''))
-
-    def rmv(self, num2=1):
-        self.num2 = num2
-        self.data -= num2
-
-        with open('flag.txt', 'w') as file:
-            file.write(f'{self.data}')
-        with open('flag.txt', 'r') as file:
-            self.data = int(file.read().replace('\n', ''))
-
-    def status(self):
-        with open('flag.txt', 'r') as file:
-            self.data = int(file.read().replace('\n', ''))
-            return self.data
-
-
-class SafePoint(Flag):
-    def __init__(self, item):
-        self.flag = Flag()
-        self.item = item
+class SearchPattern:
+    def __init__(self, pattern, lista):
+        self.pattern = pattern
+        self.lista = lista
 
     def run(self):
-        file = open('WINZ21_21-10.txt', 'a')
+        padrao = []
+        count = 1
+        for item in self.lista:
+            if item in self.pattern:
+                padrao.append(item)
+                if count == 4:
+                    print("Padrão encontrado:", padrao)
+                count += 1
+            else:
+                padrao.clear()
+                count = 1
 
-        if self.flag.status() < 2:
-            file.write(f'{self.item}, ')
-            self.flag.add()
-        else:
-            file.write('\n')
-            file.write(f'{self.item}, ')
-            self.flag.rmv()
+
+grafico_padrao = [1, 2, 1, 2]
+grafico_teste = [1, 2, 1, 2, 4, 8, 16, 32, 1, 2, 1, 2, 1, 2]
+
+teste = SearchPattern(grafico_padrao, grafico_teste)
+teste.run()
