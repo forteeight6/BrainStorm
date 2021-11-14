14e7eddee1f0ad83da89e7dc95ff22cd42001bb9
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-11-14 08:10:46 -0300

    Add files via upload

--- solucao_1.py
+++ solucao_1.py
@@ -1,40 +1,15 @@
-# Referencia:
-# https://youtu.be/KOvgRXZUWVw
+# Fechamento a cada 5 minutos
+grafico_padrao = [1, 2, 1, 2]
+grafico_teste = [1, 2, 1, 2, 4, 8, 16, 32, 1, 2, 1, 2, 1, 2]
 
-from multiprocessing import Process
-from random import shuffle, choice
-from time import sleep
-
-
-def selection(lista, quantidade):
-    while True:
-        escolhidos = []
-        for a in range(quantidade):
-            shuffle(lista)
-            while True:
-                escolha = choice(lista)
-                if escolha not in escolhidos:
-                    escolhidos.append(escolha)
-                    break
-        sleep(5)
-        print(escolhidos)
-        escolhidos.clear()
-
-
-if __name__ == '__main__':
-
-    lista = [1, 2, 3]
-    # selection(lista, 2)
-
-    processo_1 = Process(target=selection, args=(lista, 2, ))
-
-    processo_2 = Process(target=selection, args=(lista, 1, ))
-
-    processo_1.start()
-    processo_2.start()
-
-    sleep(30)
-    processo_1.terminate()
-
-    sleep(30)
-    processo_2.terminate()
+padrao = []
+count = 1
+for item in grafico_teste:
+    if item in grafico_padrao:
+        padrao.append(item)
+        if count == 4:
+            print("Padrão encontrado:", padrao)
+        count += 1
+    else:
+        padrao.clear()
+        count = 1
