34ad7fb2f7188eb6771a14df5a347e8d30577625
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-11-17 17:28:41 -0300

    Add files via upload

--- solucao_1.py
+++ solucao_1.py
@@ -7,6 +7,10 @@ import sys
 # Função adicionar padrao
 
 
+def remover():
+    pass
+
+
 def adicionar(nome, obj_map):
     padrao = []
     nome = str(nome)
@@ -17,10 +21,13 @@ def adicionar(nome, obj_map):
 
     if nome == '':
         nome = 'vazio'
-
-    with open('padroes_em_teste.txt', 'r') as file:
-        for item in file:
-            cont += 1
+    try:
+        with open('padroes_em_teste.txt', 'r') as file:
+            for item in file:
+                cont += 1
+    except:
+        open('padroes_de_teste.txt', 'x').close()
+        adicionar(nome, obj_map)
 
     with open('padroes_em_teste.txt', 'a') as file:
         file.write(str(cont+1))
@@ -32,43 +39,92 @@ def adicionar(nome, obj_map):
 
 
 def listar():
-    with open('padroes_em_teste.txt', 'r') as file:
-        for linha in file:
-            print(linha)
+    try:
+        with open('padroes_em_teste.txt', 'r') as file:
+            for linha in file:
+                print(linha)
+    except:
+        open('padroes_em_teste.txt', 'x').close()
+        listar()
 
 
 def SelecionarPorId(identificador) -> int:
-    with open('padroes_em_teste.txt', 'r') as file:
-        for linha in file:
-            dado = int(linha.split()[0])
-            if dado == identificador:
-                print('Este padrão foi selecionado:', linha)
-                return linha
+    try:
+        with open('padroes_em_teste.txt', 'r') as file:
+            for linha in file:
+                dados = []
+                cont = 0
+                dado = int(linha.split()[0])
+                if dado == identificador:
+                    print('Este padrão foi selecionado:', linha)
+                    separar = linha.replace('[', '')
+                    separar = separar.replace(']', '')
+                    separar = separar.replace(',', '')
+                    separar = separar.split()
+
+                    while True:
+                        if cont > 3:
+                            dados.append(int(separar[cont]))
+
+                        if cont == len(separar) - 1:
+                            break
+                        cont += 1
+
+                    return dados
+
+    except:
+        open('padroes_em_teste.txt', 'x').close()
+        SelecionarPorId(identificador)
 
 
 def SelecionarPorNome(nome) -> str:
     dados = []
     count = 0
-    with open('padroes_em_teste.txt', 'r') as file:
-        for linha in file:
-            dado = int(linha.split()[0])
-            dados.append(linha.split()[2])
-            if dados[dado-1] == nome:
-                print(linha)
-                count += 1
-        if count > 1:
-            opcao = int(input('Agora selecione por ID: '))
-            SelecionarPorId(opcao)
+    try:
+        with open('padroes_em_teste.txt', 'r') as file:
+            for linha in file:
+                dado = int(linha.split()[0])
+                dados.append(linha.split()[2])
+                if dados[dado-1] == nome:
+                    print(linha)
+                    count += 1
+            if count > 1:
+                opcao = int(input('Agora selecione por ID: '))
+                SelecionarPorId(opcao)
+    except:
+        open('padroes_em_teste.txt', 'x').close()
+        SelecionarPorNome(nome)
+
+
+class Padrao:
+    def __init__(self, padrao):
+        print('ou s -> sim ou n -> nao')
+        escolha = str(input()).lower()
+        if escolha == 's':
+            with open('padrao_setado.txt', 'w') as file:
+                try:
+                    file.write(str(padrao))
+
+                except:
+                    print('Falhou em Setar o Padrão.')
+        else:
+            print('O padrao não foi setado.')
 
 
 """
 class Padrao:
-    def __init__(self, padrao) -> list():
+    def setar(self, padrao):
+        print('Entrou')
         self.padrao = padrao
-
-    def setar(self):
-        print('s -> sim e n -> nao')
+        print('ou s -> sim ou n -> nao')
         escolha = str(input()).lower()
-        if 's' in escolha:
-            with open('padroes_em_teste.txt', 'r') as file:
+        if escolha == 's':
+            with open('padrao_setado.txt', 'w') as file:
+                try:
+                    file.write(str(self.padrao))
+
+                except:
+                    print('Falhou em Setar o Padrão.')
+        else:
+            print('O padrao não foi setado.')
 """
