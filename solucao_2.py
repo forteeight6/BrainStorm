aa5eacf3e9613f743542f15d38c84b28aec5b37f
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-11-17 17:29:11 -0300

    Add files via upload

--- solucao_2.py
+++ solucao_2.py
@@ -1,9 +1,14 @@
 import solucao_1 as s1
+from solucao_1 import Padrao
+from os import system
+import solucao_4 as s4
+
 
 while True:
     print('--Entre com 0 para escolher um padrão--')
     print('--Entre com 1 para adicionar um padrão--')
-    print('--Entre com 2 para sair do menu--')
+    print('--Entre com 2 para plotar o grafico--')
+    print('--Entre com 3 para sair do menu--')
     menu = int(input())
 
     if menu == 0:
@@ -15,21 +20,39 @@ while True:
             print('--Entre com 3 para voltar--')
             sub_menu = int(input())
             if sub_menu == 2:
+                system('cls')
                 s1.listar()
 
             if sub_menu == 0:
-                identificador = int(input('Digite o identificador: '))
-                s1.SelecionarPorId(identificador)
+                system('cls')
+                while True:
+                    try:
+                        identificador = int(input('Digite o identificador: '))
+                        retorno = s1.SelecionarPorId(identificador)
+                        Padrao(retorno)
+                    except:
+                        break
             elif sub_menu == 1:
+                system('cls')
                 nome = str(input('Digite o nome: '))
                 s1.SelecionarPorNome(nome)
             elif sub_menu == 3:
                 break
 
     elif menu == 1:
-        nome = input('(Opcional) Nomeie o padrao: ')
-        print('Exemplo: 5 8 9')
-        padrao = input()
-        s1.adicionar(nome, padrao)
+        system('cls')
+        while True:
+            try:
+                print('Aperte Enter para sair')
+                nome = input('(Opcional) Nomeie o padrao: ')
+                print('Exemplo: 5 8 9')
+                padrao = input()
+                s1.adicionar(nome, padrao)
+                break
+            except:
+                pass
     elif menu == 2:
+        s4.plotar()
+    elif menu == 3:
+        system('cls')
         break
