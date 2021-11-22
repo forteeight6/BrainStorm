e4a5b8221d641184c014e2eb35dd000cd98e0324
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-11-22 17:01:36 -0300

    Add files via upload

---
+++ classe_grafico.py
@@ -0,0 +1,17 @@
+# Ira pegar os dados do modelo
+class Grafico:
+    def __init__(self) -> None:
+        self.window = 'open'
+        # Adicionar os Atributos aqui.
+        pass
+
+    def atualizar_Grafico(self):
+        # Atualizar os gráficos aqui.
+        pass
+
+    def show(self):
+        # Inicializar os gráficos aqui
+        pass
+
+    def close(self):
+        pass
