88e433b373f3ba954b407a3bf977657f70c0fbbc
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-11-22 17:00:57 -0300

    Add files via upload

--- classe_edita_modelo.py
+++ classe_edita_modelo.py
@@ -11,29 +11,34 @@ class update:
                 dados1.append(linha.split())
 
         self.array = np.array(dados1)
-        # print(array)
-        # array[1, 1] = 'Ollywer'
-        # print(array)
+        # print(self.array)
+        # self.array[5][2] = True
+        # print(self.array)
 
-    def show_graphc(self, opção=False):
+    def update_graphc(self, linha, opção=False, especial=True):
         if opção:
-            self.array[0][2] = True
-            with open(self.arquivo, 'w') as file:
-                for indice, linha in enumerate(self.array):
-                    # print(linha)
-                    for indice2, item in enumerate(linha):
+            self.array[linha][2] = True
+        else:
+            self.array[linha][2] = False
+        # print(self.array)
 
-                        if indice2 == 0:
-                            file.write(self.array[indice][indice2])
-                        elif indice2 < 2:
-                            file.write(' ')
-                            file.write(self.array[indice][indice2])
-                            file.write(' ')
+        with open(self.arquivo, 'w') as file:
+            for a in self.array:
+                for cont, b in enumerate(a):
+                    if cont == 0:
+                        file.write(str(b))
+                    elif cont == 1:
+                        file.write(' ')
+                        file.write(str(b))
+                        file.write(' ')
+                    elif cont == 2:
+                        if especial:
+                            file.write(str(b))
+                            file.write('\n')
                         else:
-                            file.write(self.array[indice][indice2])
-                            file.write(' ')
+                            file.write(str(False))
                             file.write('\n')
 
 
-teste = update('modelo.py')
-teste.show_graphc(True)
+# teste = update('modelo.py')
+# teste.update_graphc(7, True, False)
