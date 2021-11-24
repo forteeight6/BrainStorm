5a8d4e89de7dfd1d79720fbe0d75c2cc6fe362f5
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-11-24 17:35:55 -0300

    Add files via upload

---
+++ classe_directory - Copia (2).py
@@ -0,0 +1,39 @@
+from os import mkdir
+import numpy as np
+from datetime import date
+
+data_atual = str(date.today())
+padrao = 'C:/Users/dyego/Desktop/OpTrade/assets/camada8/'
+diretorios = np.array(
+    [[padrao, data_atual], ['/Trash', '/WorkSpace']])
+
+cont = 0
+cont2 = 0
+concatenar = str()
+
+while True:
+
+    while True:
+        # print(diretorios[cont][cont2])
+        if cont % 2 == 0:
+            # print(diretorios[cont][cont2])
+            concatenar += diretorios[cont][cont2]
+            try:
+                mkdir(concatenar)
+            except FileExistsError as e:
+                print(f"Pasta {concatenar} já existe.")
+        elif cont % 2 != 0:
+            destino = concatenar + diretorios[cont][cont2]
+            try:
+                mkdir(destino)
+            except FileExistsError as e:
+                print(f"Pasta {destino} já existe.")
+
+        if cont2 == len(diretorios[cont]) - 1:
+            cont2 = 0
+            break
+        cont2 += 1
+
+    if cont == len(diretorios) - 1:
+        break
+    cont += 1
