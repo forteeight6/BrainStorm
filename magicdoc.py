bf8973885abb29a5273ce35c06c6476ded9d2635
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-11-27 10:01:41 -0300

    Add files via upload

--- magicdoc.py
+++ magicdoc.py
@@ -1,6 +1,20 @@
+import numpy as np
+import json as js
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.animation as anima
+from time import sleep, time
+from datetime import datetime
 # Manipulação de Documentos: Leitura, Escrita, Sobreescrita
-# O parametro time é para a função ser ativado o comando após um tempo
-# O parametro condicao é para a função ser ativada quando ficar True
+# O parametro <time> é para a função ser ativado após um tempo determinado, na lógica o parametro da condição sera trocado para True.
+# O parametro <condicao> é para a função ser ativada quando ficar True, pode se conbinar com diversas estrategias lógicas para trocar para True(Alem do proprio temporizador embutido).
+
+# O parametro <anima> Cria um grafico que se atualiza em tempo real.
+# O parametro <thread> Permite a criação de um subprocesso unico.
+# O parametro <multiprocessing> Permite a criação de multiplos subprocessos.
+# O parametro <manager> permite que o multiprocessing retorne uma variavel compartilha com os valores do subprocesso.
+# O parametro <config> sera responsavel pela inserção de um doc em json responsavel pela configuração do objeto, essa configuração pode ser atualizada pelo método AutoUpdate.
+
 
 class MagicDoc:
     def __init__(self, arquivo):
@@ -21,6 +35,8 @@ class MagicDoc:
             pass
         elif self.formatLeitura.lower() == 'array':
             pass
+        elif self.formatLeitura.lower() == 'dataframe':
+            pass
 
         if close:
             self.arquivo.close()
@@ -39,6 +55,8 @@ class MagicDoc:
         elif self.formatEscrita.lower() == 'set':
             pass
         elif self.formatEscrita.lower() == 'array':
+            pass
+        elif self.formatEscrita.lower() == 'dataframe':
             pass
 
         if close:
@@ -59,6 +77,8 @@ class MagicDoc:
             pass
         elif self.formatSobrescrita.lower() == 'array':
             pass
+        elif self.formatSobrescrita.lower() == 'dataframe':
+            pass
 
         if close:
             self.arquivo.close()
@@ -78,12 +98,26 @@ class MagicDoc:
             pass
         elif self.formatLereEscrever.lower() == 'array':
             pass
+        elif self.formatLereEscrever.lower() == 'dataframe':
+            pass
 
         if close:
             self.arquivo.close()
 
-    def AutoUpdate(self, time=None, condicao=False):
+    # AutoUpate:
+    # -- Quando executado verifica no doc permissoes(json) se o parametro referente ao arquivo monitorado esta True ( o parametro status altera a permissão), caso sim, o arquivo monitorado(self.arquivo) sera alterado pelo que estiver no parametro modificador
+    def AutoUpdate(self, status=False, modificador=None, time=None, condicao=False):
         pass
 
     def SpecialClose(self, time=None, condicao=False):
         self.arquivo.close()
+
+    # Lembre-se do projeto de real data time.
+    # Doc contendo valores atualizaveis ex:Dados Financeiros, que serão usado no gráfico.
+    def DocToGraphic(self, config=None, anima=False, thread=False, multiprocessing=False, manager=False, time=None, condicao=False):
+        pass
+
+    # Lembre-se do projeto de Geolocalização
+    # Doc contendo valores atualizaveis ex:Longitude/Latitude que serão usado no mapa.
+    def DocToMap(self, config=None, anima=False, thread=False, multiprocessing=False, manager=False, time=None, condicao=False):
+        pass
