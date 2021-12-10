0dae4e60e8bae85c2dbe83233933926e9b8cd7b5
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2021-12-10 09:43:30 -0300

    Add files via upload

---
+++ basic_testado/plot_testado.py
@@ -0,0 +1,29 @@
+"""
+Testado
+"""
+"""
+==========
+plot(x, y)
+==========
+
+See `~matplotlib.axes.Axes.plot`.
+"""
+
+
+# plt.style.use('_mpl-gallery')
+
+# make data
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 10, 100)
+y = 4 + 2 * np.sin(2 * x)
+
+# plot
+fig, ax = plt.subplots()
+
+ax.plot(x, y, linewidth=2.0)
+
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+
+plt.show()
