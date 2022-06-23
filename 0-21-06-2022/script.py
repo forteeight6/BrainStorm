9f823bf6d27b441741207e9e59a495ba92b73284
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2022-06-23 16:14:01 -0300

    Add files via upload

--- 0-21-06-2022/script.py
+++ 0-21-06-2022/script.py
@@ -1,35 +1,47 @@
-def pattern_1():
-	op = True
-	if op:
-		return True
-	else:
-		return False
+import random
 
-def pattern_2():
-	op = True
-	if op:
-		return True
-	else:
-		return False
+class Analyzer:
 
-def pattern_3():
-	op = True
-	if op:
-		return True
-	else:
-		return False
+	def __init__(self, function_list, special='dropSearch'):
+		self.function_list = function_list
+		eval('self.' + special + '()')
 
+	def _function_list(self):	
+		for function in self.function_list:
+			yield eval(function + '()')
 
-def function(function_list):
-	for function in function_list:
-		yield eval(function + '()')
+	def dropSearch(self):
+		while len(self.function_list) != 0:
+			for i, f in enumerate(self._function_list()):
+				print(f)
+				if f:
+					self.function_list.pop(i)
+
+	def resetSearch(self):
+		pass
 
 if __name__ == '__main__':
 
+	def pattern_1():
+		op = random.choice([True, False, False, False, False])
+		if op:
+			return True
+		else:
+			return False
+
+	def pattern_2():
+		op = random.choice([True, False, False, False, False])
+		if op:
+			return True
+		else:
+			return False
+
+	def pattern_3():
+		op = random.choice([True, False, False, False, False])
+		if op:
+			return True
+		else:
+			return False
+
 	function_list = ['pattern_1', 'pattern_2', 'pattern_3']
-	
-	while len(function_list) != 0:
-		for i in function(function_list):
-			print(i)
-			if i:
-				function_list.pop()
+	Analyzer(function_list)
